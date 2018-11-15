f1c807fc6c1f77770af4f2f7722e8256f68bf68d
abhilash <X107107@avidxchange.com>
2018-11-15 02:58:00 -0500

    changes in powershell script

--- publishPickledoc.ps1
+++ publishPickledoc.ps1
@@ -1,14 +1,15 @@
 # Setup variables
-$FeatureDirectory = "$PSScriptRoot\framework\Features"
-$OutputDirectory = "$PSScriptRoot\framework\Reports\Livingdocumentation"
+$Root = "C:\Users\X107107\.jenkins\workspace\AvidXchange.POC.Test\"
+$FeatureDirectory = "$Root\framework\Features"
+$OutputDirectory = "$Root\framework\Reports\Livingdocumentation"
 $DocumentationFormat = "dhtml"
 $SystemUnderTestName = "AvidXchange"
 $SystemUnderTestVersion = "1.3.42"
 $TestResultsFormat = "nunit3"
-$TestResultsFile = "$PSScriptRoot\TestResult.xml"
+$TestResultsFile = "$Root\TestResult.xml"
 
 # Import the Pickles-comandlet
-Import-Module .\packages\Pickles.2.20.1\tools\PicklesDoc.Pickles.PowerShell.dll
+Import-Module $Root\packages\Pickles.2.20.1\tools\PicklesDoc.Pickles.PowerShell.dll
 
 # Call pickles
 Pickle-Features -FeatureDirectory $FeatureDirectory  `
